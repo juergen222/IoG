5e84af23ccb79b3574ae290bf880e6e73cb9243b
juergen222 <57132494+juergen222@users.noreply.github.com>
2021-02-14 17:28:26 +0100

    Initial Commit

--- php/devices.php
+++ php/devices.php
@@ -1,6 +1,58 @@
 <?php
 include('../smarty/libs/Smarty.class.php');
 
-$smarty = new Smarty;
+$dbhost = "localhost";
+$dbuser = "projekt";
+$dbpasswd = "IOG";
+$dbname = "beet_datenbank";
 
+$smarty = new Smarty;
+$mysqli = new mysqli($dbhost, $dbuser, $dbpasswd, $dbname);
+if ($mysqli->connect_errno) {
+    // Ausgabe im Fehlerfall sollte verbessert werden :-(
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+/*$sql = "SELECT messwert.Wert, messwert.Date, messwert.StationID, messtation.name, messtation.comment, messtation.M_Id, parameter.Name, parameter.Einheit FROM messwert
+                INNER JOIN messtation
+                ON messwert.StationID = messtation.M_Id
+                INNER JOIN parameter
+                ON messwert.Parameter_Id = parameter.P_Id
+                ORDER BY messwert.Date DESC ";*/
+
+$sql = "SELECT name, comment, M_Id FROM messtation";
+$result = $mysqli->query($sql);
+
+if($result->num_rows >0){
+    echo "<div class='row'>";
+    while($row = $result->fetch_assoc())
+    {
+
+        echo "<div class='col-lg-4 col-md-4 col-sm-6 col-12 stationItem p-0 m-2'> ";
+
+
+        echo "<h3 class='h3 m-0 p-0'> " .$row["name"]." <br> <span class='information'>".$row["M_Id"] . " </span> </h3>";
+
+
+        echo  "<p>" .$row["comment"] . "</p";
+
+       echo "<button  class='btn btn-dark' >";
+
+
+        echo "</div> ";
+
+
+
+
+
+
+
+
+    }
+    echo "</div> ";
+
+}
+else{
+    echo "No Device yet added";
+}
 $smarty->display('../templates/devices.tpl');
--- templates/devices.tpl
+++ templates/devices.tpl
@@ -1,55 +1,23 @@
 {extends file="../templates/parent.tpl"}
 {block name=title}IoG - Devices{/block}
 {block name=stylesheet}<link rel="stylesheet" href="../css/deviceNav.css">{/block}
-{block name=header}<h1 class="devicesTitle">My Devices</h1>{/block}
+{block name=header}{/block}
 
 {block name=main}
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+
+
+    <div class="container p-3 bg-white " style="max-width: 80%">
+        <div class="container">
+            <h1 class="devicesTitle">My Devices</h1>
+            <p>Choose a span of days, in which you would like to see your measurements</p>
+            <form METHOD="post">
+            <label for="dayOfMeasureStart">Day Start</label>
+            <input type="date" id="dayOfMeasureStart" name="MeasureDateStart">
+            <label for="dayOfMeasureEnd">Day End</label>
+            <input type="date" id="dayOfMeasureEnd" name="MeasureDateEnd">
+
+
+        </form>
+        </div>
+    </div>
 {/block}
